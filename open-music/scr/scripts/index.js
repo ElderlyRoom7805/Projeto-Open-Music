2e052f352e9d1c79f03ada422186a3ce7147c58b
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2024-11-18 11:58:52 -0300

    "Tarefa 4 concluída"

--- open-music/scr/scripts/index.js
+++ open-music/scr/scripts/index.js
@@ -1,15 +1,19 @@
 import { applyInputRangeStyle } from "./inputRange.js";
 import { albumList } from "./albumsDatabase.js";
+import { darkOrLightMode } from "./theme.js";
 applyInputRangeStyle()
+const albumDiv = document.querySelector(".album__div");
+function removeLi() {
+    let li = document.getElementsByClassName("album__item");
+    let liLength = li.length;
+    for(let i=0; i<liLength; i++){
+        albumDiv.removeChild(li[0]);
+    }
+} 
+removeLi()
 function createCard(lista){
-    const albumDiv = document.querySelector(".album__div")
     for(let i=0; i<lista.length; i++){
-        console.log(lista[i].title);
-        console.log(lista[i].genre);
-        console.log(lista[i].band);
-        console.log(lista[i].price);
-        console.log(lista[i].img);
-        const divJs = document.createElement("div");
+        const liJs = document.createElement("li");
         const imgJs = document.createElement("img");
         const h3Js = document.createElement("h3");
         const divJsD = document.createElement("div");
@@ -19,15 +23,16 @@ function createCard(lista){
         const pJsP = document.createElement("p");
         const buttonJs = document.createElement("button");
         
-        divJs.classList.add("album__item");
-        h3Js.classList.add("albums__titles");
+        
+        liJs.classList.add("album__item");
         h3Js.classList.add("h3-L");
-        divJsD.classList.add("albums__description");
         pJsN.classList.add("album__names-L");
         pJsT.classList.add("album__types-L");
-        divjsD2.classList.add("albums__description");
         pJsP.classList.add("album__p-L");
         buttonJs.classList.add("album__button-L");
+        h3Js.classList.add("albums__titles");
+        divJsD.classList.add("albums__description");
+        divjsD2.classList.add("albums__description");
         
         imgJs.src = lista[i].img;
         h3Js.innerText = lista[i].title;
@@ -41,12 +46,120 @@ function createCard(lista){
         divjsD2.appendChild(pJsP);
         divjsD2.appendChild(buttonJs);
         
-        divJs.appendChild(imgJs);
-        divJs.appendChild(h3Js);
-        divJs.appendChild(divJsD);
-        divJs.appendChild(divjsD2);
+        liJs.appendChild(imgJs);
+        liJs.appendChild(h3Js);
+        liJs.appendChild(divJsD);
+        liJs.appendChild(divjsD2);
         
-        albumDiv.appendChild(divJs)
+        albumDiv.appendChild(liJs)
     }
 }
-createCard(albumList)
+
+createCard(albumList);
+
+const buttonAll = document.querySelector("#todos");
+const buttonPop = document.querySelector("#pop");
+const buttonMPB = document.querySelector("#mpb");
+const buttonForro = document.querySelector("#forro");
+const buttonSamba = document.querySelector("#samba");
+const buttonBaiao = document.querySelector("#baiao");
+const buttonRap = document.querySelector("#rap");
+const buttonHipHop = document.querySelector("#hiphop");
+function createFilterListbyGenres(list, valorInput) {
+    removeLi()
+    const valorMinuscula = valorInput.toLowerCase();
+    const filterList = list.filter((element, index) => {
+        const elementMenor = element.genre.toLowerCase();
+        const input = elementMenor.includes(valorMinuscula);
+        console.log(input)
+        return input
+    })
+    createCard(filterList)
+}
+buttonPop.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "pop");
+    verificaCorDeCriacao()
+})
+buttonMPB.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "MPB")
+    verificaCorDeCriacao()
+})
+buttonForro.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "Forro")
+    verificaCorDeCriacao()
+})
+buttonSamba.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "Samba")
+    verificaCorDeCriacao()
+})
+buttonBaiao.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "Baiao")
+    verificaCorDeCriacao()
+})
+buttonRap.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "Rap")
+    verificaCorDeCriacao()
+})
+buttonHipHop.addEventListener("click", function (e) {
+    createFilterListbyGenres(albumList, "Hip-Hop")
+    verificaCorDeCriacao()
+})
+buttonAll.addEventListener("click", function (e) {
+    removeLi();
+    createCard(albumList);
+    verificaCorDeCriacao()
+})
+
+const inputRange = document.getElementById("input-range");
+let inputValue = document.getElementById("input-value");
+
+function createFilterListByPrice(list) {
+    removeLi()
+    let inputValueNow = inputRange.value;
+    const filterList = list.filter((element, index) => {
+        let elementPrice = element.price;
+        let elementFixed = parseInt(elementPrice);
+        return elementFixed <= inputValueNow
+    })
+    createCard(filterList);
+    
+}
+inputRange.addEventListener("input", (e) => {
+    inputValue.innerText = inputRange.value;
+    createFilterListByPrice(albumList)
+})
+
+function verificaCorDeCriacao() {
+    let darkVerificacao = JSON.parse(localStorage.getItem("@openMusic:theme"))
+    console.log(darkVerificacao)
+    if (darkVerificacao) {
+        const albumItem = document.querySelectorAll(".album__item");
+        for(let i=0; i<albumItem.length; i++){
+            albumItem[i].classList.toggle("album__item-D");
+        }
+        const h3Album = document.querySelectorAll(".h3-L");
+        for(let i= 0; i<h3Album.length; i++){
+            h3Album[i].classList.toggle("h3-D");
+        }
+        const namesAlbum = document.querySelectorAll(".album__names-L");
+        for(let i= 0; i<namesAlbum.length; i++){
+            namesAlbum[i].classList.toggle("album__names-D");
+        }
+        const typesAlbum = document.querySelectorAll(".album__types-L");
+        for(let i= 0; i<typesAlbum.length; i++){
+            typesAlbum[i].classList.toggle("album__types-D");
+        }
+        const pAlbum = document.querySelectorAll(".album__p-L");
+        for(let i= 0; i<pAlbum.length; i++){
+            pAlbum[i].classList.toggle("album__p-D");
+        }
+        const buttonAlbum = document.querySelectorAll(".album__button-L");
+        for(let i= 0; i<buttonAlbum.length; i++){
+            buttonAlbum[i].classList.toggle("album__button-D");
+        }
+        return console.log("precisou")
+    } else {
+        return console.log("não precisou")
+    }
+}
+darkOrLightMode()
---
+++ open-music/scr/scripts/theme.js
@@ -0,0 +1,70 @@
+export function darkOrLightMode(){
+    const buttonChange = document.querySelector(".header__change");
+
+    function themeChange(){
+        const bodyDark = document.querySelector(".body-L");
+        bodyDark.classList.toggle("body-D");
+        
+        const headDark = document.querySelector(".head-L");
+        headDark.classList.toggle("head-D");
+        
+        const h1Dark = document.querySelector(".header__h1-L");
+        h1Dark.classList.toggle("header__h1-D");
+        
+        const spanDark = document.querySelector(".span-L");
+        spanDark.classList.toggle("span-D");
+        
+        const changeDark = document.querySelector(".L");
+        changeDark.classList.toggle("D");
+        
+        const divNamesDark = document.querySelectorAll(".div__names-L");
+        for(let i=0; i<divNamesDark.length; i++){
+            divNamesDark[i].classList.toggle("div__names-D");
+        }
+        
+        const h2Genre = document.querySelectorAll(".h2-L");
+        for(let i=0; i<h2Genre.length; i++){
+            h2Genre[i].classList.toggle("h2-D");
+        }
+        
+        const albumItem = document.querySelectorAll(".album__item");
+        for(let i=0; i<albumItem.length; i++){
+            albumItem[i].classList.toggle("album__item-D");
+        }
+        const h3Album = document.querySelectorAll(".h3-L");
+        for(let i= 0; i<h3Album.length; i++){
+            h3Album[i].classList.toggle("h3-D");
+        }
+        const namesAlbum = document.querySelectorAll(".album__names-L");
+        for(let i= 0; i<namesAlbum.length; i++){
+            namesAlbum[i].classList.toggle("album__names-D");
+        }
+        const typesAlbum = document.querySelectorAll(".album__types-L");
+        for(let i= 0; i<typesAlbum.length; i++){
+            typesAlbum[i].classList.toggle("album__types-D");
+        }
+        const pAlbum = document.querySelectorAll(".album__p-L");
+        for(let i= 0; i<pAlbum.length; i++){
+            pAlbum[i].classList.toggle("album__p-D");
+        }
+        const buttonAlbum = document.querySelectorAll(".album__button-L");
+        for(let i= 0; i<buttonAlbum.length; i++){
+            buttonAlbum[i].classList.toggle("album__button-D");
+        }
+    }
+    let darkMode = false;
+
+    function localStorageChanged() {
+        darkMode = JSON.parse(localStorage.getItem("@openMusic:theme"));
+        if(darkMode){
+            return themeChange();
+        }
+    }
+
+    buttonChange.addEventListener("click", function(e){
+        darkMode = !darkMode;
+        localStorage.setItem("@openMusic:theme", JSON.stringify(darkMode));
+        themeChange();
+    })
+    localStorageChanged()
+}
